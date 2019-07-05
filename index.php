7ca10e734da796bbef6423c103763efe0a264041
Grandpere <lorenzo.marozzo@gmail.com>
2019-07-05 15:21:04 +0200

    first version without responsive

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lorenzo Marozzo - Développeur Web et Web Mobile spécialisé Symfony</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <header class="navbar-fixed">
+        <nav>
+            <ul id="navbar" class="nav">
+                <li class="nav-item"><a class="nav-link" href="#resume">Accueil</a></li>
+                <li class="nav-item"><a class="nav-link" href="#presentation">Présentation</a></li>
+                <li class="nav-item"><a class="nav-link" href="#experiences">Expériences</a></li>
+                <li class="nav-item"><a class="nav-link" href="#competences">Compétences</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="resume">
+            <h1>Lorenzo Marozzo</h1>
+            <h2>Développeur Web spécialisé Symfony</h2>
+            <h3>Coulommiers (77) - Ile de France</h3>
+        </section>
+        <div class="container">       
+        <section id="presentation">
+            <h1>Présentation</h1>
+            <p>
+                Je m'appelle Lorenzo Marozzo, je suis issu d'une formation informatique néanmoins je n'ai jamais eu la possibilité de mettre cela en pratique. Après avoir passé 8 ans dans le secteur e-commerce notamment au sein d'un service clientèle j'ai décidé de revenir dans le développement.
+            </p>
+            <p>
+                Je me suis donc lancé dans l'aventure O'Clock où j'ai vécu pendant 4 mois une formation intensive de développeur web. Cette formation était suivi d'un mois de projet en équipe pendant lequel j'ai pu mettre en application ce que j'ai appris lors de la formation.
+            </p>
+            <p>
+                Aujourd'hui je cherche à mettre en pratique toutes les connaissances que j'ai acquises pendant cette formation.
+            </p>
+            <a class="btn btn-lg btn-outline-success" href="assets/files/Marozzo-Lorenzo.pdf" target="_blank" data-toggle="tooltip" title="Télécharger mon CV au format PDF dans une nouvelle fenêtre du navigateur">Télécharger mon CV</a>
+        </section>
+        </div>
+        <section id="experiences">
+            <h1>Expériences</h1>
+            <article class="experiences-wrapper">
+                <img class="experiences-image" src="assets/images/ochildren.png" alt="illustration de la page d'accueil du site O'Children">
+                <div class="experiences-infos">
+                    <h2>O'Children</h2>
+                    <ul class="experiences-tags">
+                        <li><span class="badge badge-secondary">Symfony4</span></li>
+                        <li><span class="badge badge-secondary">Doctrine</span></li>
+                        <li><span class="badge badge-secondary">MySQL</span></li>
+                        <li><span class="badge badge-secondary">JWT</span></li>
+                        <li><span class="badge badge-secondary">API REST</span></li>
+                    </ul>
+                    <p>
+                        Projet réalisé en équipe pour la fin de formation O'clock. Le but étant de réaliser un site fonctionnel en un mois après 4 mois de formation intensive.
+                        O'children est un site fait en deux parties. J’ai réalisé la partie Back (API et back-office) à l'aide du Framework Symfony, la partie Front a été réalisé par mes collègues en React . La liaison avec la base de données s’est faite grâce à Axios et mon API développé sans l'aide de bundle de type APIPlatform, FosRestBundle, etc...
+                    </p>
+                    <a class="btn btn-lg btn-outline-primary" href="https://github.com/Grandpere/O-Children-API" target="_blank" data-toggle="tooltip" title="Accéder au dépôt Back-End sur Github dans une nouvelle fenêtre du navigateur">Voir le dépot Back-End</a>
+                    <a class="btn btn-lg btn-outline-secondary" href="https://github.com/Grandpere/O-Children" target="_blank" data-toggle="tooltip" title="Accéder au dépôt Front-End sur Github dans une nouvelle fenêtre du navigateur">Voir le dépot Front-End</a>
+                </div>
+            </article>
+            <div class="separator"></div>
+            <article class="experiences-wrapper">
+                <div class="experiences-infos">
+                    <h2>Lorem, ipsum dolor.</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nobis eveniet consequatur neque culpa! Mollitia magnam aliquid dolorum deleniti. Libero totam earum suscipit quisquam exercitationem ea natus sapiente minima accusantium neque inventore, expedita quaerat est illo commodi eaque sed facere?
+                    </p>
+                    <a class="btn btn-lg btn-outline-primary" href="" target="_blank" data-toggle="tooltip" title="Voir le site">Voir le site</a>
+                </div>
+                <img class="experiences-image" src="https://picsum.photos/500/300" alt="illustration du site">
+            </article>
+            <div class="separator"></div>
+            <article class="experiences-wrapper">
+                <img class="experiences-image" src="https://picsum.photos/500/300" alt="illustration du site">
+                <div class="experiences-infos">
+                    <h2>Lorem ipsum dolor sit.</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quasi itaque laborum neque architecto consectetur quae! Officiis ducimus provident ab similique repellat, praesentium fuga fugit nemo, voluptas debitis nulla vitae facere eum consequuntur, illo rem enim voluptate repellendus? Id deleniti laboriosam soluta pariatur! Vel in doloremque unde illum, ullam aliquam.
+                    </p>
+                    <a class="btn btn-lg btn-outline-primary" href="" target="_blank" data-toggle="tooltip" title="Voir le site">Voir le site</a>
+                </div>
+            </article>
+            <div class="separator"></div>
+            <article class="experiences-wrapper">
+                <div class="experiences-infos">
+                    <h2>Lorem ipsum dolor sit.</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quasi itaque laborum neque architecto consectetur quae! Officiis ducimus provident ab similique repellat, praesentium fuga fugit nemo, voluptas debitis nulla vitae facere eum consequuntur, illo rem enim voluptate repellendus? Id deleniti laboriosam soluta pariatur! Vel in doloremque unde illum, ullam aliquam.
+                    </p>
+                    <a class="btn btn-lg btn-outline-primary" href="" target="_blank" data-toggle="tooltip" title="Voir le site">Voir le site</a>
+                </div>
+                <img class="experiences-image" src="https://picsum.photos/500/300" alt="illustration du site">
+            </article>
+        </section>
+        <div class="container">
+        <section id="competences">
+            <h1>Compétences</h1>
+            <div class="row justify-content-center">
+                <div class="card">
+                    <img src="assets/images/html5.svg" class="card-img-top" alt="Logo de la technologie HTML5">
+                    <p class="card-title">HTML5</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/css3.svg" class="card-img-top" alt="Logo de la technologie CSS3">
+                    <p class="card-title">CSS3</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/js.svg" class="card-img-top" alt="Logo de la technologie JavaScript">
+                    <p class="card-title">JavaScript</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/php.svg" class="card-img-top" alt="Logo de la technologie PHP">
+                    <p class="card-title">PHP</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/symfony.svg" class="card-img-top" alt="Logo de la technologie Symfony">
+                    <p class="card-title">Symfony</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/twig.png" class="card-img-top" alt="Logo de la technologie Twig">
+                    <p class="card-title">Twig</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/mysql.svg" class="card-img-top" alt="Logo de la technologie MySQL">
+                    <p class="card-title">mySQL</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/git.svg" class="card-img-top" alt="Logo de la technologie Git">
+                    <p class="card-title">Git</p>
+                </div>
+                <div class="card">
+                    <img src="assets/images/github.svg" class="card-img-top" alt="Logo de la technologie Github">
+                    <p class="card-title">Github</p>
+                </div>
+                <div class="card" data-toggle="tooltip" title="Opquast certified : Maîtrise de la qualité en projet Web - 915 points">
+                    <a href="https://directory.opquast.com/fr/certificat/0CMUEW/"><img src="https://res.cloudinary.com/opquast/image/upload/c_fill,h_130,w_130/v1/badges/expert.svg" alt="logo représentant le niveau de certification Opquast de Lorenzo Marozzo"></a>
+                    <p class="card-title">Opquast</p>
+                </div>                         
+            </div>
+        </section>
+        <section id="contact">
+            <h1>Me contacter</h1>
+            <div class="row justify-content-around">
+                <div class="card">
+                    <a href="https://www.linkedin.com/in/lorenzomarozzo/"><img src="assets/images/linkedin.svg" alt="Lien vers le profil LinkedIn de Lorenzo Marozzo" data-toggle="tooltip" title="Lien vers le profil LinkedIn de Lorenzo Marozzo"></a>
+                    <p class="card-title text-center mt-2">LinkedIn</p>
+                </div>
+                <div class="card">
+                    <a href="mailto:lorenzo.marozzo@gmail.com&subject=contact via le portfolio"><img src="assets/images/mail.svg" alt="Envoyer un mail à Lorenzo Marozzo" data-toggle="tooltip" title="Envoyer un mail à Lorenzo Marozzo"></a>
+                    <p class="card-title text-center mt-2">Email</p>
+                </div>
+                <div class="card">
+                    <a href="tel:+33698326414"><img src="assets/images/phone.svg" alt="Téléphoner à Lorenzo Marozzo" data-toggle="tooltip" title="Téléphoner à Lorenzo Marozzo"></a>
+                    <p class="card-title text-center mt-2">Téléphone</p>
+                </div>
+            </div>
+        </section>
+        </div>
+    </main>
+    <footer>
+        <!-- <a href="">Mentions légales</a> -->
+        <p>
+            2019 © <strong>Lorenzo Marozzo</strong>. Tous droits réservés.
+        </p>
+    </footer>
+
+</body>
+
+</html>
