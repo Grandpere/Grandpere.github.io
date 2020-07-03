1181ea71680f1c0383c1b3c104eb2d2d6af11652
Grandpere <lorenzo.marozzo@gmail.com>
2020-07-04 00:41:55 +0200

    improve html with contact form + update css + improve js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,35 @@
 let app = {
     init: function () {
         document.getElementById("year").innerHTML = new Date().getFullYear().toString();
+
+        document.querySelector("#contact-me-button").addEventListener('click', function (event) {
+            app.send(event);
+        });
+    },
+    send: function (event) {
+        event.preventDefault();
+
+        let name = document.querySelector("#contact-me-name").value;
+        let email = document.querySelector("#contact-me-email").value;
+        let content = document.querySelector("#contact-me-message").value;
+
+        Email.send({
+            SecureToken : "c20cdf47-f67e-453e-8dbb-7e76cf8d0206",
+            To : 'lorenzo.marozzo@gmail.com',
+            From : email,
+            Subject : name + " - Contact from portfolio",
+            Body : content + "<br />" + email
+        }).then(
+            function (message) {
+                document.querySelector("#contact-me-error").innerText = message;
+                document.querySelector("#contact-me-error").classList.remove("hide");
+                document.querySelector("#contact-me-error").classList.add("show");
+            },
+            setInterval(function () {
+                document.querySelector("#contact-me-error").classList.remove("show");
+                document.querySelector("#contact-me-error").classList.add("hide");
+            }, 4000),
+        );
     }
 }
 
